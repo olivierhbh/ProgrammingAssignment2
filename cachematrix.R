ad4d46a2210bad9733519a333f85347b5ee89713
Olivier Hui Bon Hoa <olivier.huibonhoa@gmail.com>
2016-03-13 23:03:25 +0100

    added body functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,19 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Function that can cache the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+      mat <- NULL
+      set <- function(y) {
+            x <<- y
+            mat <<- NULL
+      }
+      get <- function() x
+      setInvMat <- function(invMat) mat <<- invMat
+      getInvMat <- function() mat
+      list(set = set, get = get,
+           setInvMat = setInvMat,
+           getInvMat = getInvMat)
 }
 
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      invMat <- x$getInvMat()
+      if(!is.null(invMat)) {
+            message("getting cached data")
+            return(invMat)
+      }
+      data <- x$get()
+      invMat <- solve(data, ...)
+      x$setInvMat(invMat)
+      invMat
+      
 }
